f392e963b43fdce10649828126680845578c6d71
simonmeza01 <simon.meza@correo.unimet.edu.ve>
2022-06-16 22:35:39 -0400

    Interfaz bonita

--- Proyecto2_MetroMendeley/src/Main.java
+++ Proyecto2_MetroMendeley/src/Main.java
@@ -15,6 +15,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("PRUEBA");
         System.out.println("UWUUUUUU");
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
     }
     
 }
